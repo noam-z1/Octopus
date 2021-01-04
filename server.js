f8af62fd7f85baffbf9374455ed735d13a65a24c
Noam Zadok <noam.z1@cs.huji.ac.il>
2021-01-04 12:13:15 +0200

    Update server.js
    Added get users, get users/:id, post users methods

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 const Hapi = require('@hapi/hapi');
+const uuid = require('uuid');
 
 let users;
 
-const init = async () => {
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+server.route({
+    method: 'GET',
+        path: '/users',
+        handler: (req, res) => {
+            return res.response(users).code(400);
+        }
+})
+
+server.route({
+    method: 'GET',
+        path: '/users/{id}',
+        handler: (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const getIndex = users => users.id === id;
+            const index = users.findIndex(getIndex);
+            if (index == -1){
+                return res.response("Id not found").code(400);
+            }
+            return res.response(users[index]).code(200);
+        }
+})
+
+server.route({
+    method: 'POST',
+        path: '/users',
+        handler: (req, res) => {
+            const id = uuid.v1();
+            const data = req.payload;
+            if (!data){
+                return res.response("Please enter user data").code(400);
+            }
+            if (!(data.name && data.team)){
+                return res.response("Missing data").code(400);
+            }
+            let user = {id:id,name:data.name, team: data.team}
+            users.push(user);
+            return res.response(user).code(200);
+        }
+})
+
+
+const init = async () => {
 
     users = [];
 
@@ -16,3 +59,4 @@ const init = async () => {
 };
 
 init();
+
